9da377dad66b51ecd28b091951111a1537cb8ab2
Woolley <bwoolley@UMICH.EDU>
2026-07-29 11:17:00 -0400

    Fix invalid True boolean crashing main.js, breaking AOS animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,7 +204,7 @@
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: True
+      disableOnInteraction: true
     },
     pagination: {
       el: '.swiper-pagination',
